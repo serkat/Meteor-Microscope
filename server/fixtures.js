232ced8b7fcbd58482bff0c5270fd3287cfdb5d9
Sergio <sgresr@gmail.com>
2017-08-24 22:10:00 +0200

    meteor added data to the posts collection

---
+++ server/fixtures.js
@@ -0,0 +1,18 @@
+if ( Posts.find().count() === 0 ) {
+
+    Posts.insert({
+        title: 'Introducing Telescope',
+        url: "example.com"
+    });
+
+    Posts.insert({
+        title: 'Meteor',
+        url: "http://meteor.com"
+    });
+
+    Posts.insert({
+        title: 'The Meteor book',
+        url: "http:// themeteorbook.com"
+    });
+
+}
